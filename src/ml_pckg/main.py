152e137a1f8abe583622ab05171691a701cf0a7e
Imran Ansari <stasim101@gmail.com>
2023-03-10 13:11:46 +0530

    Histogram

--- src/ml_pckg/main.py
+++ src/ml_pckg/main.py
@@ -19,9 +19,10 @@ def main(filename):
     y_std = numpy.std(y)
     y_len = len(y)
 
-    y_a = numpy.random.normal(y_mean, y_std, y_len)
+#    y_a = numpy.random.normal(y_mean, y_std, y_len)
 
-    plot.scatter(x, y_a)
+#    plot.scatter(x, y_a)
+    plot.hist(x, y_len)
     plot.title('Walmart Sales Analysis')
     plot.xlabel('Categories')
     plot.ylabel('Sales Count')
