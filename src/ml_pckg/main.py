28815a40f69a30284147fcedebaa3d5ff0ef1eee
Imran Ansari <stasim101@gmail.com>
2023-03-10 16:16:14 +0530

    Polynomial Regression

--- src/ml_pckg/main.py
+++ src/ml_pckg/main.py
@@ -1,23 +1,18 @@
+import numpy
 import matplotlib.pyplot as plt
 from scipy import stats
 
 x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
 y = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
 
-slope, intercept, r, p, std_err = stats.linregress(x, y)
-
-
-def myfunc(x):
-    return slope * x + intercept
-
 
 def main():
-    model = list(map(myfunc, x))
+    model = numpy.poly1d(numpy.polyfit(x, y, 3))
+    line = numpy.linspace(1, 22, 100)
     plt.scatter(x, y)
-    plt.plot(x, model)
+    plt.plot(line, model(line))
     plt.show()
 
 
 if __name__ == '__main__':
     main()
-    
